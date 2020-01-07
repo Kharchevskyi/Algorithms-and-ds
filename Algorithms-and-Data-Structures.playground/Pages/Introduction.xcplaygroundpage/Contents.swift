8abcb8fa4fc9fba0659b5b03ba9b40e67f72be97
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-01-07 16:36:21 +0100

    Add Stack and Queue

---
+++ Algorithms-and-Data-Structures.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+ [Home](Introduction)
+
+ # Introduction
+ 
+ Swif playground Page used to learn algorithms and data structures.
+ Easy way to organize and structurize information while learning and improving
+ basics about algorithms and data structures
+ 
+ ## Table of Contents
+ * [Linked List](Linked%20List)
+ * [Stack](Stack)
+
+ 
+ [< Previous](@previous)           [Next >](@next)
+ */
