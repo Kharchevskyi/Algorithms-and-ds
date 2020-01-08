b7a24fda5ec57e8ffe070eab124716304be27730
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-01-08 10:16:00 +0100

    Add Introduction page table of contents

--- Algorithms-and-Data-Structures.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Algorithms-and-Data-Structures.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -8,10 +8,27 @@
  basics about algorithms and data structures
  
  ## Table of Contents
+ 
+ * Data Structures
  * [Basics](Basics)
  * [Linked List](Linked%20List)
  * [Stack](Stack)
  * [Queue](Queue)
+ * Trees 
+ * Binary Tree
+ * Priority Queue
+ 
+ ## Algorithms
+ * Merge Sort
+ * Quick Sort
+ * Merge Sort
+ * Binary Search
+ * Rdix Sort
+ * Heap Sort
+ * Breadth-First Search
+ * Depth-First Search
+ * Dijkstra’s Algorithm
+ * Prim’s Algorithm
  
  [< Previous](@previous)           [Next >](@next)
  */
