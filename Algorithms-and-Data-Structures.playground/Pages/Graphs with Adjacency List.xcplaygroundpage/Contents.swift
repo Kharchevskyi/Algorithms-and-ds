a62e6d3b28c71a62ce320ce9326e67e4778f86b8
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2021-07-22 18:14:36 +0300

    Graphs with Adjacency List

--- Algorithms-and-Data-Structures.playground/Pages/Graphs with Adjacency List.xcplaygroundpage/Contents.swift
+++ Algorithms-and-Data-Structures.playground/Pages/Graphs with Adjacency List.xcplaygroundpage/Contents.swift
@@ -1,24 +1,132 @@
 //: [Previous](@previous)
+//: https://www.raywenderlich.com/773-swift-algorithm-club-graphs-with-adjacency-list
 
 import Foundation
 
-public struct Vertex<T: Hashable>: Hashable, CustomStringConvertible {
-    public var description: String {
+// MARK: - Vertex
+struct Vertex<T: Hashable>: Hashable, CustomStringConvertible {
+    var description: String {
         "\(data)"
     }
     var data: T
 }
 
-public enum EdgeType {
+// MARK: - Edge
+enum EdgeType {
     case directed, undirected
 }
 
-public struct Edge<T: Hashable> {
-    public var source: Vertex<T>
-    public var destination: Vertex<T>
-    public let weight: Double?
+struct Edge<T: Hashable>: Hashable  {
+    var source: Vertex<T>
+    var destination: Vertex<T>
+    let weight: Double?
 }
 
+// MARK: - Graph
+protocol Graphable {
+    associatedtype Element: Hashable
+    
+    
+    func createVertex(data: Element) -> Vertex<Element>
+    func add(_ type: EdgeType, from source: Vertex<Element>, to destination: Vertex<Element>, weight: Double?)
+    func weight(from source: Vertex<Element>, to destination: Vertex<Element>) -> Double?
+    func edges(from source: Vertex<Element>) -> [Edge<Element>]?
+    
+    var description: CustomStringConvertible { get }
+}
 
+// MARK: - Adjacency List
+open class AdjacencyList<T: Hashable>: Graphable {
+    typealias Element = T
+    
+    var adjacencyDict : [Vertex<T>: [Edge<T>]] = [:]
+    
+    var description: CustomStringConvertible {
+        var result = ""
+        for (vertex, edges) in adjacencyDict {
+            var edgeString = ""
+            for (index, edge) in edges.enumerated() {
+                if index != edges.count - 1 {
+                    edgeString.append("\(edge.destination), ")
+                } else {
+                    edgeString.append("\(edge.destination)")
+                }
+            }
+            result.append("\(vertex) ---> [ \(edgeString) ] \n ")
+        }
+        return result
+    }
+    
+    init() {}
+    
+    func createVertex(data: Element) -> Vertex<Element> {
+        let vertex = Vertex(data: data)
+        
+        if adjacencyDict[vertex] == nil {
+            adjacencyDict[vertex] = []
+        }
+        
+        return vertex
+    }
+    
+    func add(_ type: EdgeType, from source: Vertex<Element>, to destination: Vertex<Element>, weight: Double?) {
+        switch type {
+        case .directed:
+            addDirectedEdge(from: source, to: destination, weight: weight)
+        case .undirected:
+            addUndirectedEdge(vertices: (source, destination), weight: weight)
+        }
+    }
+    
+    private func addDirectedEdge(from source: Vertex<Element>, to destination: Vertex<Element>, weight: Double?) {
+        let edge = Edge(source: source, destination: destination, weight: weight)
+        adjacencyDict[source]?.append(edge)
+    }
+    
+    private func addUndirectedEdge(vertices: (Vertex<Element>, Vertex<Element>), weight: Double?) {
+        let (source, destination) = vertices
+        addDirectedEdge(from: source, to: destination, weight: weight)
+        addDirectedEdge(from: destination, to: source, weight: weight)
+    }
+    
+    func weight(from source: Vertex<Element>, to destination: Vertex<Element>) -> Double? {
+        adjacencyDict[source]?
+            .filter { $0.destination == destination }
+            .first?
+            .weight
+    }
+    
+    func edges(from source: Vertex<Element>) -> [Edge<Element>]? {
+        adjacencyDict[source]
+    }
+}
+
+// MARK: - Example
+
+let adjacencyList = AdjacencyList<String>()
+
+let singapore = adjacencyList.createVertex(data: "Singapore")
+let tokyo = adjacencyList.createVertex(data: "Tokyo")
+let hongKong = adjacencyList.createVertex(data: "Hong Kong")
+let detroit = adjacencyList.createVertex(data: "Detroit")
+let sanFrancisco = adjacencyList.createVertex(data: "San Francisco")
+let washingtonDC = adjacencyList.createVertex(data: "Washington DC")
+let austinTexas = adjacencyList.createVertex(data: "Austin Texas")
+let seattle = adjacencyList.createVertex(data: "Seattle")
+
+adjacencyList.add(.undirected, from: singapore, to: hongKong, weight: 300)
+adjacencyList.add(.undirected, from: singapore, to: tokyo, weight: 500)
+adjacencyList.add(.undirected, from: hongKong, to: tokyo, weight: 250)
+adjacencyList.add(.undirected, from: tokyo, to: detroit, weight: 450)
+adjacencyList.add(.undirected, from: tokyo, to: washingtonDC, weight: 300)
+adjacencyList.add(.undirected, from: hongKong, to: sanFrancisco, weight: 600)
+adjacencyList.add(.undirected, from: detroit, to: austinTexas, weight: 50)
+adjacencyList.add(.undirected, from: austinTexas, to: washingtonDC, weight: 292)
+adjacencyList.add(.undirected, from: sanFrancisco, to: washingtonDC, weight: 337)
+adjacencyList.add(.undirected, from: washingtonDC, to: seattle, weight: 277)
+adjacencyList.add(.undirected, from: sanFrancisco, to: seattle, weight: 218)
+adjacencyList.add(.undirected, from: austinTexas, to: sanFrancisco, weight: 297)
+
+print(adjacencyList.description)
 
 //: [Next](@next)
